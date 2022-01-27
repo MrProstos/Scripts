57b6175bb0136978d7c7b6e76073ddf3332fbaa4
MrProstos <92064304+MrProstos@users.noreply.github.com>
2022-01-27 22:25:07 +0200

    Update Wireguard_addUser.sh

--- Wireguard_addUser.sh
+++ Wireguard_addUser.sh
@@ -1,47 +1,50 @@
 #!/bin/bash
 
 read -p "Enter the path to file.txt -> " File
-read -p "Enter name group -> " DirName
 
-if ! [[ -f $DirName ]]
+if [[ -e $File ]]
 then
-    mkdir $DirName
-fi
 
-PublicServerKey=$(cat /etc/wireguard/publickey)
+    if ! [[ -f $GroupName ]]
+    then
+        mkdir $GroupName
+    fi
 
-while IFS= read -r line
-do
-    
-    IdUser=$(echo $line | cut -d "," -f1)
-    NameUser=$(echo $line | cut -d "," -f2-3)
+    PublicServerKey=$(cat /etc/wireguard/publickey)
 
-    mkdir $NameUser/$NameUser/
+    while IFS= read -r line
+    do
 
-    wg genkey | tee /home/$DirName/$NameUser/private-key | wg pubkey |  tee /home/$DirName/$NameUser/public-key
-    
-    PrivateKey=$(cat /home/$DirName/$NameUser/private-key)
-    PublicKey=$(cat /home/$DirName/$NameUser/public-key)
+        IdUser=$(echo $line | cut -d "," -f1)
+        NameUser=$(echo $line | cut -d "," -f2-3)
 
-    echo "[Interface]
+        mkdir $NameUser/$NameUser/
+
+        wg genkey | tee /home/$GroupName/$NameUser/private-key | wg pubkey |  tee /home/$GroupName/$NameUser/public-key
+
+        PrivateKey=$(cat /home/$GroupName/$NameUser/private-key)
+        PublicKey=$(cat /home/$GroupName/$NameUser/public-key)
+
+        echo "[Interface]
 Address=10.0.0.$IdUser/24
 PrivateKey=$PrivateKey
 DNS=8.8.8.8
-    
 [Peer]
 PublicKey=$PublicServerKey
 AllowedIPs=0.0.0.0/0
 EndPoint=81.20.147.218:30000
 PersistentKeepalive=20" > $NameUser.txt
 
-zip $NameUser.zip $NameUser.txt
-rm $NameUser.txt
-mv $NameUser.zip $DirName
+        zip $NameUser.zip $NameUser.txt                                                                                                                                                                                    rm $NameUser.txt
+        mv $NameUser.zip $GroupName
 
-echo "
+        echo "
 [Peer]
 PublicKey=$PublicKey
-AllowedIPs=10.0.0.$IdUser/24
-" >> /etc/wireguard/wg0.conf
+AllowedIPs=10.0.0.$IdUser/24" >> /etc/wireguard/wg0.conf
 
-done < $File
+    done < $File
+    mv $GroupName /etc/wireguard/
+else
+    echo "File not found!"
+fi
